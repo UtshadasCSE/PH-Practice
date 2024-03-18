98482e3c5342b18d46dcb267620c066954418a9b
utsha das <utshachandradas@gmail.com>
2024-03-18 16:56:31 +0600

    Image search project done

--- Image Search/Js/script.js
+++ Image Search/Js/script.js
@@ -3,40 +3,46 @@ const accessKey = "3wfqrUBY-a18LAZ5w6nQ0wDgMlPJ14BZLG4sPuhf7mU";
 
 // find all element from html file
 const formEl = document.getElementById("form");
-const inputEl = document.getElementById("inputField");
+
 const searchResultContainer = document.getElementById("searchResultContainer");
 const searchBtn = document.getElementById("searchBtn");
 const showMoreBtn = document.getElementById("showMoreBtn");
-const searchIItem = inputEl.value;
-const fetchApiData = async () => {
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${searchIItem}&client_id=${accessKey}`;
+
+const fetchApiData = async (search) => {
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${search}&client_id=${accessKey}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
-
-  showDataDisplay(data);
+  const results = data.results;
+  console.log(results);
+  showDataDisplay(results);
 };
-fetchApiData();
-const showDataDisplay = (data) => {
-  data.map((element) => {
-    const containerDiv = document.createElement("div");
-    containerDiv.classList(
-      "grid grid-cols-3 max-sm:grid-cols-2 gap-5 w-4/5 max-sm:w-11/12"
-    );
+
+const showDataDisplay = (datas) => {
+  const containerData = document.getElementById("searchResultContainer");
+
+  containerData.textContent = "";
+
+  datas.slice(0, 9).forEach((element) => {
     const underContainerDiv = document.createElement("div");
     underContainerDiv.innerHTML = `
-            <div class="flex flex-col gap-7">
+            <div class="flex w-full flex-col gap-4 ">
             <img
-              class="w-full rounded-xl shadow-2xl shadow-[#535C91]"
-              src="${element.results.urls.small}"
+              class="w-[100%] h-[200px]  rounded-xl shadow-2xl shadow-[#535C91]"
+              src="${element.urls.small}"
               alt=""
             />
-            <h3 class="text-white text-xl font-bold">$${element.results.alt_description}</h3>
+            <h3 class="text-white text-xl font-bold">${element.alt_description.slice(
+              0,
+              20
+            )}</h3>
           </div>
     `;
-    containerDiv.appendChild(underContainerDiv);
+    containerData.appendChild(underContainerDiv);
   });
+  // containerData.appendChild(containerDiv);
 };
 searchBtn.addEventListener("click", () => {
-  fetchApiData();
+  const inputEl = document.getElementById("inputField");
+  const searchIItem = inputEl.value;
+  fetchApiData(searchIItem);
 });
